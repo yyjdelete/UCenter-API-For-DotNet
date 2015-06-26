ed19a432d9c3ed8c04b781c48409ece98f4fbb26
SilverFox <yyjdelete@126.com>
2015-06-26 13:10:24 +0800

    实现IDisposable来释放httpClient

--- src/DS.Web.UCenter/Client/UcClientBase.cs
+++ src/DS.Web.UCenter/Client/UcClientBase.cs
@@ -19,6 +19,9 @@ namespace DS.Web.UCenter.Client
     /// mailto:dozer.cc@gmail.com
     /// </summary>
     public abstract class UcClientBase
+#if NET4_5
+        : IDisposable
+#endif
     {
         /// <summary>
         /// 得到加密后的input参数
@@ -303,6 +306,43 @@ namespace DS.Web.UCenter.Client
                 return base.SendAsync(request, cancellationToken);
             }
         }
+
+        #region IDisposable Support
+        private bool disposedValue = false; // To detect redundant calls
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: dispose managed state (managed objects).
+                    if (httpClient != null)
+                        httpClient.Dispose();
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
+                // TODO: set large fields to null.
+
+                disposedValue = true;
+            }
+        }
+
+        // TODO: override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources.
+        // ~UcClientBase() {
+        //   // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+        //   Dispose(false);
+        // }
+
+        // This code added to correctly implement the disposable pattern.
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+            Dispose(true);
+            // TODO: uncomment the following line if the finalizer is overridden above.
+            // GC.SuppressFinalize(this);
+        }
+        #endregion
 #endif
     }
 }
